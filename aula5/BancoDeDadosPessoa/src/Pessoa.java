3a89d2c605f48a73959291b5c7e7ef2d46314f24
Gustavo Brand <41551014+gustavobrand@users.noreply.github.com>
2026-03-27 20:27:03 -0300

    Principio de resp. unica (#2)

---
+++ aula5/BancoDeDadosPessoa/src/Pessoa.java
@@ -0,0 +1,53 @@
+import java.util.Objects;
+
+public class Pessoa {
+
+	private String nome;
+	private String cpf;
+
+	public Pessoa(String nome, String cpf) {
+		this.nome = nome;
+		this.cpf = cpf;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getCpf() {
+		return cpf;
+	}
+
+	public void setCpf(String cpf) {
+		this.cpf = cpf;
+	}
+
+	@Override
+	public String toString() {
+		return "Pessoa{" +
+				"nome='" + nome + '\'' +
+				", cpf='" + cpf + '\'' +
+				'}';
+	}
+
+	@Override
+	public boolean equals(Object o) {
+		if (this == o) {
+			return true;
+		}
+		if (o == null || getClass() != o.getClass()) {
+			return false;
+		}
+		Pessoa pessoa = (Pessoa) o;
+		return nome.equalsIgnoreCase(pessoa.nome);
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(nome);
+	}
+}
